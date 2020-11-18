75df6de8564701d9d6d496027b0d2ee53a59363f
Maria Belen Ceron <mabelenceron@gmail.com>
2020-11-18 11:02:07 -0500

    Eliminando archivos innecesarios

--- deber_Papi/5_ejercicio.py
+++
@@ -1,8 +0,0 @@
-numero = int(input('Ingrese el numero de N para formar la matriz identidad: '))
-for i in range(numero):
-    for j in range(numero):
-        if i == j:
-            print('1', end=" ")
-        else:
-            print('0',end=" ")
-    print()
